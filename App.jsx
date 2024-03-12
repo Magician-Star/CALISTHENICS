d40cc438ecf705071a229518064fe3d4984a39fe
biel <104595280+Magician-Star@users.noreply.github.com>
2024-03-12 13:34:02 +0000

    Novas modificações

--- App.jsx
+++ App.jsx
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { ScrollView, TextInput } from 'react-native-web';
 
 const nome = 'Gabriel'
 export default function App() {
   return (
     
     <View style={styles.container}>
-      <h1>App criado com React-Native</h1>
-      <Text>Eu me chamo {nome} </Text>
-      <TextInput style={{height:40, borderColor:'gray', borderWidth: 1, }} defaultValue="Escreva qualquer coisa aqui..." />
+      <ScrollView>
+        <h1>Projeto Calibuilder</h1>
+        <Text>Eu me chamo {nome} </Text>
+        <TextInput style={{height:40, borderColor:'gray', borderWidth: 1, }} defaultValue="Escreva qualquer coisa aqui..." />
+        
+        <StatusBar style="auto" />
       
-      <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
