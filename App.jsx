73fb3ec953fb2b5cbd052b3ede81916dedd00105
biel <104595280+Magician-Star@users.noreply.github.com>
2024-03-12 18:20:06 +0000

    appjsx

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from 'react-navigation-stack';
 
 function HomeScreen() {
   return (
